feb9a1a524fd1d9c9d198a0ffa23c7a4f03973d8
camille <huangcamillej@gmail.com>
2024-06-15 20:02:32 -0700

    fixed

--- src/Components/SkillsCards.jsx
+++ src/Components/SkillsCards.jsx
@@ -39,7 +39,7 @@ const SkillsCards = () => {
               <span
                 className="text-xl"
                 style={{
-                  fontFamily: "Poppins, sans-sreif",
+                  fontFamily: "Poppins, sans-serif",
                   fontWeight: "bold",
                 }}
               >
