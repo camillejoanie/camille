aafce287a0a7d489583bb7088048a10f29654f95
camille <huangcamillej@gmail.com>
2024-08-10 20:04:52 -0700

    spring

--- src/Components/HeroText.jsx
+++ src/Components/HeroText.jsx
@@ -21,7 +21,7 @@ const HeroText = () => {
         <m.h1
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
-          transition={{ duration: 0.6, type: "s" }}
+          transition={{ duration: 0.6, type: "spring" }}
           className="text-center flex justify-center items-center flex-col opacity-100 text-7xl sm:text-9xl cursor-default"
           style={{ fontFamily: "SuperMario" }}
         >
