1e7c7436df53ad15501a6fef8988b63e3257533b
camille <huangcamillej@gmail.com>
2024-08-30 22:59:57 -0700

    sprin

--- src/Components/HeroText.jsx
+++ src/Components/HeroText.jsx
@@ -35,7 +35,7 @@ const HeroText = () => {
           <m.div
             initial={{ x: 100 }}
             whileInView={{ x: 0 }}
-            transition={{ duration: 0.6, type: "spri" }}
+            transition={{ duration: 0.6, type: "sprin" }}
           >
             {name.split("").map((char, index) => {
               if (char === " ") {
