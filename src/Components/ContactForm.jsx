1abbd7ba6afd6bc3b5a863b065001052c1211b81
camille <huangcamillej@gmail.com>
2024-09-28 13:43:09 -0700

    messa

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -100,7 +100,7 @@ const ContactForm = () => {
             <textarea
               className="p-[0.5em] rounded-xl text-grayscale-950"
               placeholder="Enter your message..."
-              name="messag"
+              name="messa"
               required
             ></textarea>
           </div>
