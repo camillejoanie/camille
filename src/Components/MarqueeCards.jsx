34f1f708f8a0c69efa96f96d87c07c230b6bf40b
camille <huangcamillej@gmail.com>
2024-10-19 16:23:22 -0700

    marquee

--- src/Components/MarqueeCards.jsx
+++ src/Components/MarqueeCards.jsx
@@ -7,7 +7,7 @@
    Last modification: 03/06/2023
 */
 
-import Marquee from "react-fast-m";
+import Marquee from "react-fast-marquee";
 
 const MarqueeCards = ({ children, direction }) => {
   return (
