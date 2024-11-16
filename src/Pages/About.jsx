940ad13aaec794bc40f3284aecfaad45a78d315e
camille <huangcamillej@gmail.com>
2024-11-16 01:42:22 -0800

    hey

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -45,6 +45,7 @@ const About = () => {
           <div className="w-full md:w-[50%] flex h-full items-center justify-center">
             <div className="w-[80%] lg:w-[50%] h-[300px] sm:h-[350px] flex justify-center items-center">
               <ImageSlider images={memoji.image} />
+              hey
             </div>
           </div>
         </div>
