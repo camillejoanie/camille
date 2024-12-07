5f68a5ac85a24f9f86752fcc6741f65747c2638e
camille <huangcamillej@gmail.com>
2024-12-07 12:03:08 -0800

    hateeeeee

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -43,7 +43,7 @@ const About = () => {
             </LazyMotion>
           </div>
           <div className="w-full md:w-[50%] flex h-full items-center justify-center">
-            <div className="w-[80%] lg:w-[50%] h-[300px] sm:h-[350px] flex justify-center item-center">
+            <div className="w-[80%] lg:w-[50%] h-[300px] sm:h-[350px] flex justify-center items-center">
               <ImageSlider images={memoji.image} />
             </div>
           </div>
