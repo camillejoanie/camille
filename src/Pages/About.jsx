d4bfdb6bf770ffdc7aca0634ead64a56ddf9574c
camille <huangcamillej@gmail.com>
2024-12-07 12:03:48 -0800

    get meeeeee

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -38,7 +38,7 @@ const About = () => {
                 <span className="text-primary-400">{introduction.text[0]}</span>
                 <span>{introduction.text[1]}</span>
                 <span>{introduction.text[2]}</span>
-                <span>{introduction.text[3]}</span>
+                <span>{introduction.text[3]}</spa>
               </m.p>
             </LazyMotion>
           </div>
