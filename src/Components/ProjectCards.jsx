399e4a70211c5e229caaaddb83bd0e0a35354024
camille <huangcamillej@gmail.com>
2024-12-12 23:18:35 -0800

    haneun

--- src/Components/ProjectCards.jsx
+++ src/Components/ProjectCards.jsx
@@ -36,7 +36,7 @@ export const textVariant = (delay) => {
       opacity: 1,
       transition: {
         type: "spring",
-        duration: 1.25,
+        duration: 1.2,
         delay: delay,
       },
     },
