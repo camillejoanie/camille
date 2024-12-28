37e0c86524b125514a3680d9066f6a2e5654422c
camille <huangcamillej@gmail.com>
2024-12-27 18:46:47 -0800

    tracking

--- src/Components/SectionTitle.jsx
+++ src/Components/SectionTitle.jsx
@@ -31,7 +31,7 @@ const SectionTitle = (props) => {
           {subtitle}
         </span>
         <h2
-          className="tr-wider text-7xl sm:text-8xl md:text-9xl"
+          className="tracking-wider text-7xl sm:text-8xl md:text-9xl"
           style={{ fontFamily: "Morganite Black" }}
         >
           {title.split("").map((char, index) => {
